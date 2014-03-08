211b1f3368a2455cd958f56af79ee9e62e655e56
Ben Cox <Ben@Benjojo.co.uk>
2014-03-08 15:13:24 +0000

    Fixed issues where it would not build a "correct" wav

--- main.go
+++ main.go
@@ -4,32 +4,54 @@ import (
 	"fmt"
 	"github.com/youpy/go-wav"
 	"io"
-	"io/ioutil"
+	"log"
 	"os"
 )
 
 func main() {
 	// I have no idea what I am doing
 	fmt.Println("Bop")
-	file, _ := os.Open("./test.wav")
-	output, _ := os.Open("./out.wav")
+	if len(os.Args) <= 1 {
+		log.Fatal("You need to tell me what file to encode.")
+	}
+	file, _ := os.Open(os.Args[1])
+
+	output, _ := os.OpenFile("./out.wav", os.O_CREATE, 600)
+
 	reader := wav.NewReader(file)
 
 	// Settings for output
-	var numSamples uint32 = 2
+	var numSamples uint32 = 9999
 	var numChannels uint16 = 2
 	var sampleRate uint32 = 44100
 	var bitsPerSample uint16 = 16
+	writer := wav.NewWriter(output, numSamples, numChannels, sampleRate, bitsPerSample)
+	a := 0
 	// End of settings for output
 	for {
+		samplez := make([]wav.Sample, 0)
 		samples, err := reader.ReadSamples()
-		fmt.Println(len(samples))
+		// Samples will return (usally) 2048 samples in a single sitting
+
+		log.Println(len(samples))
 		if err == io.EOF {
 			break
 		}
 
-		// for _, sample := range samples {
-		// fmt.Printf("L/R: %d/%d\n", reader.IntValue(sample, 0), reader.IntValue(sample, 1))
-		// }
+		a++
+		for _, sample := range samples {
+			sam := wav.Sample{}
+			L := reader.IntValue(sample, 0)
+			R := reader.IntValue(sample, 1)
+
+			sam.Values[0] = L
+			sam.Values[0] = R
+			samplez = append(samplez, sam)
+
+		}
+
+		// fmt.Println(len(samplez), len(samples))
+		writer.WriteSamples(samplez)
 	}
+
 }
